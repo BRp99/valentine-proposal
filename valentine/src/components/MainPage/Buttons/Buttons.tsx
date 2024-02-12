72dc21ec60fda9630d13682ded1d96027fd107a5
BRp99 <124196319+BRp99@users.noreply.github.com>
2024-02-12 21:42:48 +0000

    Improve code inside Buttons component:
    create 2 states: isNoButtonsVisible state tocontrol visibility of btn "No" and yesButtonSize state to control size of btn "Yes";
    
    create 2 functions: handleYesClick function to handle btn "Yes" click and handleNoClick to handle btn "No" click;
    
    create an message array to show different messages when the user click on btn "No";
    
    create a messageIndex state to update with setMessageIndex function the display of messages inside message array.

--- valentine/src/components/MainPage/Buttons/Buttons.tsx
+++ valentine/src/components/MainPage/Buttons/Buttons.tsx
@@ -1,78 +1,47 @@
 import styles from "./Buttons.module.css"
 import { useState } from "react"
 
-interface Props {
-  onYesClick: () => void
-}
+const messages = [
+  "No",
+  "Are you sure?",
+  "Really sure?",
+  "Really really really sure?",
+  "Just think about it!",
+  "Are you positive?",
+  "If you say no I'll be very very sad",
+  "Really very very sad",
+  "Ok fine! I'll stop asking...",
+  "Just Kidding, PLEASE!",
+]
 
-export default function Buttons({ onYesClick }: Props) {
-  const [showNoButton, setShowNoButton] = useState(true)
+export default function Buttons() {
+  const [isNoButtonVisible, setIsNoButtonVisible] = useState(true)
 
-  const [noText, setNoText] = useState("No")
-  const [yesSize, setYesSize] = useState(1)
+  const [yesButtonSize, setYesButtonSize] = useState(15)
+
+  const [messageIndex, setMessageIndex] = useState(0)
+
+  const handleYesClick = () => {
+    setIsNoButtonVisible(false)
+  }
 
   const handleNoClick = () => {
-    switch (noText) {
-      case "No":
-        setNoText("Are you sure?")
-        setYesSize(2)
-        break
-      case "Are you sure?":
-        setNoText("Really sure?")
-        setYesSize(2.5)
-        break
-      case "Really sure?":
-        setNoText("Really really sure?")
-        setYesSize(3)
-        break
-      case "Really really sure?":
-        setNoText("Really realy really sure?")
-        setYesSize(3.5)
-        break
-      case "Really realy really sure?":
-        setNoText("Are you positive?")
-        setYesSize(4)
-        break
-      case "Are you positive?":
-        setNoText("Just think about it")
-        setYesSize(4.5)
-        break
-      case "Just think about it":
-        setNoText("If you say no I'll be very very sad")
-        setYesSize(5)
-        break
-      case "If you say no I'll be very very sad":
-        setNoText("Really very very sad")
-        setYesSize(5.5)
-        break
-      case "Really very very sad":
-        setNoText("OK fine! I'll stop asking")
-        setYesSize(6)
-        break
-      case "OK fine! I'll stop asking":
-        setNoText("Just Kidding, PLEASEEE!")
-        setYesSize(6.5)
-        break
-      default:
-        break
-    }
+    setYesButtonSize(yesButtonSize + 25)
+
+    setMessageIndex((prevIndex) => (prevIndex + 1) % messages.length)
   }
 
   return (
     <div className={styles.container}>
-      <button
-        className={styles.yes}
-        onClick={() => {
-          onYesClick()
-          setNoText("Yes")
-        }}
-        style={{ fontSize: `${yesSize}rem` }}
-      >
+      <button className={styles.yes} style={{ fontSize: `${yesButtonSize}px` }} onClick={handleYesClick}>
         Yes
       </button>
-      <button className={styles.no} onClick={handleNoClick}>
-        {noText}
-      </button>
+
+      {isNoButtonVisible && (
+        <button className={styles.no} onClick={handleNoClick}>
+          {messages[messageIndex]}
+        </button>
+      )}
     </div>
   )
 }
--- valentine/src/components/MainPage/MainPage.tsx
+++ valentine/src/components/MainPage/MainPage.tsx
@@ -2,20 +2,13 @@ import styles from "./MainPage.module.css"
 import BearAlone from "./BearAlone/BearAlone"
 import Buttons from "./Buttons/Buttons"
 import Phrase from "./Phrase/Phrase"
-import { useState } from "react"
 
 export default function MainPage() {
-  const [showSecondImage, setShowSecondImage] = useState(false)
-
-  const handleYesClick = () => {
-    setShowSecondImage(true)
-  }
-
   return (
     <div className={styles.container}>
-      <BearAlone showSecondImage={showSecondImage} />
+      <BearAlone />
       <Phrase />
-      <Buttons onYesClick={handleYesClick} />
+      <Buttons />
     </div>
   )
 }

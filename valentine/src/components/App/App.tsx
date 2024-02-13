3bad615923292f704d3fc9ea806719d049e39d18
BRp99 <124196319+BRp99@users.noreply.github.com>
2024-02-13 15:16:24 +0000

    add a new feature:
    create local state to control if stopwatch should it be displayed or not;
    
    create setInterval function to update state in every second;
    
    create calculateTimeRemaining function to calculate the time left until the end date;
    
    adding css style.

--- valentine/src/components/App/App.tsx
+++ valentine/src/components/App/App.tsx
@@ -1,9 +1,65 @@
 import MainPage from "../MainPage/MainPage"
+import { useState, useEffect } from "react"
+import styles from "./App.module.css"
 
 export default function App() {
+  const [showProject, setShowProject] = useState(false)
+  const [timeRemaining, setTimeRemaining] = useState({ hours: 0, minutes: 0, seconds: 0 })
+
+  useEffect(() => {
+    const currentDate = new Date()
+    const targetDate = new Date("2024-02-13T15:35:00")
+
+    if (currentDate >= targetDate) {
+      setShowProject(true)
+    } else {
+      const timeDiff = targetDate.getTime() - currentDate.getTime()
+      const secondsRemaining = Math.floor(timeDiff / 1000)
+
+      setTimeRemaining(calculateTimeRemaining(secondsRemaining))
+
+      const intervalId = setInterval(() => {
+        const now = new Date()
+        const secondsRemaining = Math.floor((targetDate.getTime() - now.getTime()) / 1000)
+
+        setTimeRemaining(calculateTimeRemaining(secondsRemaining))
+
+        if (now >= targetDate) {
+          setShowProject(true)
+          clearInterval(intervalId)
+        }
+      }, 1000)
+    }
+  }, [])
+
+  const calculateTimeRemaining = (seconds: number) => {
+    const hours = Math.floor(seconds / 3600)
+    const minutes = Math.floor((seconds % 3600) / 60)
+    const remainingSeconds = seconds % 60
+    return { hours, minutes, seconds: remainingSeconds }
+  }
+
   return (
     <div>
-      <MainPage />
+      {showProject ? (
+        <MainPage />
+      ) : (
+        <div className={styles.container_countdown}>
+          <h1 className={styles.title}> Valentine Proposal Project</h1>
+          <div className={styles.watch}>
+            <div>{timeRemaining.hours}</div>
+
+            <div>{timeRemaining.minutes}</div>
+            <div>{timeRemaining.seconds}</div>
+          </div>
+          <div className={styles.watch_informations}>
+            <div>Hours</div>
+
+            <div>Minutes</div>
+            <div>Seconds</div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- valentine/src/components/MainPage/Phrase/Phrase.tsx
+++ valentine/src/components/MainPage/Phrase/Phrase.tsx
@@ -3,7 +3,7 @@ import styles from "./Phrase.module.css"
 export default function Phrase() {
   return (
     <div className={styles.container}>
-      <h1>Will you be my Valentine?</h1>
+      <h2>Will you be my Valentine?</h2>
     </div>
   )
 }
